30a357de5d331adba06c2f8161fbcad5af8b6d5e
MaxGalindo150 <maximilianogalindo7@gmail.com>
2022-04-09 18:07:26 -0500

    :(

--- RK4.c
+++ RK4.c
@@ -1,36 +1,103 @@
 #include <stdio.h>
 #include <math.h>
 
-/*dy/dx = x-y = f(x,y)*/
-float f(float x, float y)
+double V(double L, double m, double r)
 {
-    return(y - x*x +1);
+    return(pow(L,2)*((1/(2*pow(r,2))) - (m/pow(r,3))));
 }
+
+/*dI1/dt = -4*I1+3*I2+6 = f1(x,y)*/
+double R(double t, double r, double phi)
+{
+    return(sqrt(2*0.46 - 2*V(5,1,r)));
+}
+
+/*dy2/dx = 0.6*f1(t,I1,I2)-0.2*I2 = f2(x,y)*/
+double Phi(double t, double r,double phi)
+{
+    return(5/sqrt(2*0.4-2*V(5,1,r)));
+}
+
 
 int main()
 {
-    int i, N;
-    float x0, y0, a, b, h, k1, k2, k3, k4;
+    
+    int i, j, N;
+    double t0, r0, phi0, a, b, h, x, y, k11, k21, k31, k41, k12, k22, k32, k42;
     printf("Type the value of a and b:\n");
-    scanf("%f%f", &a, &b);
+    scanf("%lf%lf", &a, &b);
     printf("Type the value of N:\n");
     scanf("%d", &N);
-    printf("Type the value of x0 and y0(x0):\n");
-    scanf("%f%f", &x0, &y0);
+    printf("Type the value of t0, r0(t0) and phi(t0):\n");
+    scanf("%lf%lf%lf", &t0, &r0, &phi0);
     h = (b-a)/N;
+    
     printf("h=%f\n", h);
     printf(" ");
-    printf("at x=%f\t, y=%f\t", x0, y0);
+    printf("at t=%f\t, r=%f\t, phi=%f\n", t0, r0*cos(phi0), r0*sin(phi0));
+    
     for (i=0; i <= N; i++)
     {
-        k1 = h*f(x0,y0);
-        k2 = h*f(x0 + h/2, y0 + k1/2);
-        k3 = h*f(x0 + h/2, y0 + k2/2);
-        k4 = h*f(x0 + h, y0 + k3);
-        y0 = y0 + (k1 + 2*k2 + 2*k3 + k4)/6;
-        x0 = x0 + h;
-        printf("at x=%f\t, y=%f\n", x0, y0);
+        for (j=1; j<=2; j++)
+        {
+            if (j == 1)
+            {
+                k11 = h*R(t0,r0,phi0);
+            }
+            else
+            {
+                k12 = h*Phi(t0,r0,phi0);
+            }
+        }
+        
+        for (j=1; j<=2; j++)
+        {
+            if (j == 1)
+            {
+                k21 = h*R(t0 + h/2, r0 + k11/2, phi0 + k12/2);
+            }
+            else
+            {
+                k22 = h*Phi(t0 + h/2, r0 + k11/2, phi0 + k12/2);
+            }
+        }
+        
+        for (j=1; j<=2; j++)
+        {
+            if (j == 1)
+            {
+                k31 = h*R(t0 + h/2, r0 + k21/2, phi0 + k22/2);
+            }
+            else
+            {
+                k32 = h*Phi(t0 + h/2, r0 + k21/2, phi0 + k22/2);
+            }
+        }
+        
+        for (j=1; j<=2; j++)
+        {
+            if (j == 1)
+            {
+                k41 = h*R(t0 + h/2, r0 + k31/2, phi0 + k32/2);
+            }
+            else
+            {
+                k42 = h*Phi(t0 + h/2, r0 + k31/2, phi0 + k32/2);
+            }
+
+        }
+        t0 = t0 + h;
+        r0 = r0 + (k11 + 2*k21 + 2*k31 + k41)/6;
+        phi0 = phi0 + (k12 + 2*k22 + 2*k32 + k42)/6;
+
+        x = r0*cos(phi0);
+        y = r0*sin(phi0);
+
+        printf("at t=%f\t, x=%f\t, y=%f\n", t0, x, y);
+
     }
 
     return 0;  
+        
 }
+
